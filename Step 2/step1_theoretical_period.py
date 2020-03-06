119eaa73e574d5431327f9797f1b91d37b895b5c
acremer01 <allisoncremer01@gmail.com>
2020-03-06 11:15:20 -0500

    data

--- Step 2/step1_theoretical_period.py
+++ Step 2/step1_theoretical_period.py
@@ -1,23 +1,31 @@
 # Step Two of Project 1
 # Allison and Pan
+# This file graphs the theoretical periods vs. lengths of  the pendulum
+# We worked alone on this assignment
 
+# IMPORT STATEMENTS
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
+# GLOBAL VARIABLES
 lengths=[]
 lengths_array=np.array(lengths)
 
-## ***** UNITS ***** ##
+# CUSTOM FUNCTIONS
 def swing_period(lengths):
+    # swing_period takes in an array of lengths as a parameter
+    # returns an array of periods calculated for each length
     periods=[]
     for i in range(len(lengths)):
         period=2*math.pi*math.sqrt(lengths[i]/9.8)
         periods.append(period)
-    return periods
+    periodarray=np.array(periods)
+    return periodarray
         
-# **ask about what values should be on which axis
 def graph_values(lengths,periods):
+    # graph_values takes an array of lengths and an array of periods as parameters
+    # graphs length vs period for a theoretical model
     plt.plot(periods, lengths)
     plt.axis([0,100,0,100])
     plt.show()
--- Step 4/step4_actual_period.py
+++ Step 4/step4_actual_period.py
@@ -1,21 +1,32 @@
 # IMPORT STATEMENTS
 import os
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 # GLOBAL VARIABLES
 path = "/Users/allisoncremer/Documents/GitHub/project-1-allison-and-pan/Step 3"
 
 # CUSTON FUNCTIONS
-def create_array(fin):
-    arr = np.array([])
-    for ln in fin:
-        newrow=[ln]
-        np.append(arr,newrow)
-    return arr
+def create_acceleration_array(fin):
+    array = np.loadtxt(fin, delimiter=',')
+    print(array)
+    return array
+
+
+def create_angle_array(arr):
+    array=[]
+    for info in arr:
+        denom=math.sqrt(info[2]*info[2]+info[3]*info[3])
+        num=info[1]
+        templist=[info[0],math.atan2(num,denom)]
+        array=np.append(array,templist)
+    return array
 
 
 
 # MAIN
 os.chdir(path)
-fin=open("dataexample.pdf","r",encoding='utf-8')
-print(create_array(fin))
+newarr=create_acceleration_array('35cm.csv')
+otherarr=create_angle_array(newarr)
+
